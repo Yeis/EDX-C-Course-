aa38b7cc1a61d65e063d57ff9d3723c67a2a0dbb
Jose Carlos Gallegos Romero <jcrockyeis@gmail.com>
2015-12-29 13:37:03 -0800

    Module 9 Finished and preparing to start module 10

--- EDX/Mod_9_Homework/Mod_9_Homework/MainWindow.xaml.cs
+++ EDX/Mod_9_Homework/Mod_9_Homework/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace Mod_9_Homework
     /// </summary>
     public partial class MainWindow : Window
     {
+        //PROPERTIES
+        List<Student> Students = new List<Student>();
+        int Index = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            //ad student to the list 
+            Students.Add(new Student(txtFirstName.Text, txtLastName.Text, txtCity.Text));
+            MessageBox.Show("Student Created ");
+            Index = Students.Count;
+            //Clearing TextBoxs
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtCity.Clear();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (Index == Students.Count)
+            {
+                Index = 0;
+            }
+            else
+            {
+                Index++;
+            }
+
+           
+            //Display next element
+            txtFirstName.Text = Students[Index].FirstName;
+            txtLastName.Text = Students[Index].LastName;
+            txtCity.Text = Students[Index].City;
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if (Index == 0)
+            {
+                Index = Students.Count;
+            }
+            else
+            {
+                Index--;
+            }
+            //Display previous element
+            txtFirstName.Text = Students[Index].FirstName;
+            txtLastName.Text = Students[Index].LastName;
+            txtCity.Text = Students[Index].City
         }
     }
 }
---
+++ EDX/Mod_9_Homework/Mod_9_Homework/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    public class Student
+    {
+        //properties and member variables 
+        private string firstname;
+        private string lastname;
+        private string city;
+
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        //Contrutctors 
+        public Student()
+        {
+
+        }
+        public Student(string first, string last, string city)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.City = city;
+        }
+    }
+}
